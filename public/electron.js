9f0708e8e3437ff9fd83eebd0cf6a1a80d1d6c4f
ernestognw <ernestognw@gmail.com>
2019-06-04 12:52:33 -0500

    feature(general): Add basic project setup.
    Basic project setup with live reloading both for renderer and main process is setted. It is recommended to use this boilerplate as an starter for future projects, with considerations on the order of the files and directories. Everything is working correctly.
    
    No breaking changes. Just needed to start developing :)

---
+++ public/electron.js
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import isDev from "electron-is-dev";
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  if (isDev) {
+    // Open the DevTools.
+    //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+    mainWindow.webContents.openDevTools();
+  }
+  mainWindow.on("closed", () => (mainWindow = null));
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
